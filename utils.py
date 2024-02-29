d6ab02c5d1c879f35703dd615e3630d4a9138d11
ehkfsjdi <t.k.ahola@student.vu.nl>
2024-02-29 14:27:38 +0200

    Added token write function

--- utils.py
+++ utils.py
@@ -1,9 +1,20 @@
 # Helper functions for the API
 import secrets
+import os
 
 def get_csrf_token():
     '''Create a random CSRF token'''
     return str(secrets.token_hex(32))
+
+def write_token(token: str):
+    # Write the access token into a file
+    f = open("./.token", "w")
+    f.write(token)
+    f.close()
+    # Change the file permissions to only read and write by owner
+    # This: https://www.tutorialspoint.com/How-to-change-the-permission-of-a-file-using-Python was used to determine how to change file permissions
+    os.chmod("./.token", 0o600)
+    return
 
 def format_repo_list(repos):
     '''Modify the format of repositories'''
